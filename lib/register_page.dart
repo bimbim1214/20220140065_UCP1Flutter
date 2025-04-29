9847c078953ceb4e44c5dda401e101eea1f8d046
bimbim1214 <bimoadityapangestu@gmail.com>
2025-04-29 13:41:00 +0700

    Membuat struktur awal RegisterPage dengan kontrol input untuk nama, email, telepon, dan password

---
+++ lib/register_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/login_page.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+  bool _obscurePassword = true; 
+
+
+  
+}
