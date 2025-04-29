99b1ee081cbe6689fdbc61c408d6cb0ce80c4ab1
bimbim1214 <bimoadityapangestu@gmail.com>
2025-04-29 19:29:32 +0700

    menmabahkan baris info tanggal dan nama anggota di DetailTugas

--- lib/datapiket/detail_page.dart
+++ lib/datapiket/detail_page.dart
@@ -18,5 +18,60 @@ class DetailTugasPage extends StatelessWidget {
   // }
 
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Detail Tugas',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Text(
+                    tanggal,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Text(
+                    namaAnggota,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            
+          ],
+        ),
+      ),
+    );
+  }
 }
