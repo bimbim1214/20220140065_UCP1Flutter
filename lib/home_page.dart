e2103cabf42c1b379088ab3051a073562368053d
bimbim1214 <bimoadityapangestu@gmail.com>
2025-04-29 14:44:36 +0700

    Menambahkan deklarasi awal HomePage sebagai StatefulWidget.

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:ucp1/datapelanggan/data_pelanggan_page.dart';
+import 'package:ucp1/login_page.dart';
+import 'package:ucp1/datapiket/piket_gudang_page.dart';
+import 'package:ucp1/produk/pendataan_barang_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
