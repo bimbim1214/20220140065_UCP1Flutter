c701d0b16d4f8de70b07bda14a6e0bb9faf3b31a
bimbim1214 <bimoadityapangestu@gmail.com>
2025-04-29 21:41:40 +0700

    menambah halaman detail pendataan barang dan navigasi setelah simpan data

--- lib/produk/detail_pendataan_page.dart
+++ lib/produk/detail_pendataan_page.dart
@@ -19,5 +19,80 @@ class DetailPendataanPage extends StatelessWidget {
     required this.HargaSatuan,
   });
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF7F4), 
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green[100],
+                ),
+                padding: const EdgeInsets.all(24),
+                child: Icon(Icons.check_circle, size: 80, color: Colors.green),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Data Berhasil Disimpan',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 60),
+              
+              
+              TextField('Tanggal', TanggalTransaksi),
+              const Divider(),
+              TextField('Jenis Transaksi', JenisTransaksi),
+              const Divider(),
+              TextField('Jenis Barang', JenisBarang),
+              const Divider(),
+              TextField('Jumlah Barang', JumlahBarang),
+              const Divider(),
+              TextField('Jenis Harga Satuan', 'Rp. $HargaSatuan'),
+              const Divider(),
+              TextField('Total Harga', 'Rp. $TotalHarga'),
+              const Divider(),
+
+              const Spacer(),
+
+              
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green[900],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                  ),
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false,
+                );
+                  },
+                  child: const Text(
+                    'Selesai',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  
   
 }
