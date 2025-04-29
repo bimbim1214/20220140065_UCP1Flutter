7b005215fd47c33d5d54c1002e75a15f7f1b188e
bimbim1214 <bimoadityapangestu@gmail.com>
2025-04-29 13:24:50 +0700

    Menambahkan input form email dan password dengan validasi dan ikon prefix.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -168,6 +168,7 @@ class _LoginPageState extends State<LoginPage> {
                     ],
                   )
                 ],
+                
               ),
             ),
           ),
